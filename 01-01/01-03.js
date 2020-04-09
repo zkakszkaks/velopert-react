62008424d879a40949b20250354bb664f1874609
카만카만 <zkakszkaks@gmail.com>
2020-04-09 18:21:51 +0900

    2020-04-09

---
+++ 01-01/01-03.js
@@ -0,0 +1,38 @@
+/**
+ * 작업환경 준비
+ * [01] 필요한 프로그램
+ * 1. node.js
+ * 2. yarn
+ * 3. visual studio code
+ * 4. git
+ *
+ * [02] node.js란?
+ * 1. 브라우저가 아닌 환경에서도 자바스크립트를 실행하게 해주는 자바스크립트 런타임
+ * 2. 웹팩, 바벨 같은 도구들이 node.js 기반으로 만들어져 있다.
+ * 3. node.js를 설치하면 npm(node package manager)이라는 도구가 같이 설치된다.
+ *
+ * [03] yarn
+ *  1. 자바스크립트 패키지를 매니징 하는 도구
+ *  2. npm과 비슷한 도구
+ *  3. 자바스크립트를 설치할때 npm보다 빠른 속도로 다운로드 받을 수 있게 해준다.
+ *
+ * [04] VS Code
+ *  1. 에디터
+ *
+ * [05] 각 프로그램들이 잘 설치 되었는지 확인
+ * 1. node --version
+ * 2. yarn --version
+ *
+ * [06] 터미널에서 현재 자신이 위치한 경로를 확인하는 명령어
+ * 1. pwd
+ *
+ * [06] 새로운 리액트 프로젝트 만들기
+ * 1. mkdir react-tutorial
+ * 2. cd react-tutorial
+ * 3. npx create-react-app begin-react
+ * 4. code begin-react
+ * 5. npm start(or yarn start)
+ * 6. 개발서버 종료: CTRL+C
+ *
+ *
+ */
