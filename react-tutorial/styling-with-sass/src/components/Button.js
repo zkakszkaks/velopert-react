3ccfb526c7a411e6da62d148d0b0a58e64c4eabb
카만카만 <zkakszkaks@gmail.com>
2020-04-25 16:00:13 +0900

    2020-04-25

--- react-tutorial/styling-with-sass/src/components/Button.js
+++ react-tutorial/styling-with-sass/src/components/Button.js
@@ -1,7 +1,14 @@
 import React from "react";
+import classNames from "classnames";
 import "./Button.scss";
-function Button({ children }) {
-  return <button className="button">{children}</button>;
+
+// size: large, medium, small
+// npm install classnames
+function Button({ children, size }) {
+  return <button className={classNames("Button", size)}>{children}</button>;
 }
 
+Button.defaultProps = {
+  size: "medium",
+};
 export default Button;
